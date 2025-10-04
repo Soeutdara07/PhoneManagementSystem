204369fa36c1fc9a9b677ae99ceb74bd4274d1e7
Soeutdara07 <darasoeut977@gmail.com>
2025-10-04 14:54:32 +0700

    hi

--- resources/views/back_end/brand.blade.php
+++ resources/views/back_end/brand.blade.php
@@ -26,7 +26,7 @@
                                 <th>Brand ID</th>
                                 <th>Name</th>
                                 <th>Status</th>
-                                <th>Action</th>
+                                <th>Actionsssssss</th>
                             </tr>
                         </thead>
                         <tbody class="brand_list">
